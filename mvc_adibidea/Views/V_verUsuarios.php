27efce43da74844e87ebe7f3db86e5629e7cb385
Enaut AA <enaut.irakasle@gmail.com>
2024-09-18 17:31:45 +0200

    modifikar eta eliminar linkak gehitu

--- mvc_adibidea/Views/V_verUsuarios.php
+++ mvc_adibidea/Views/V_verUsuarios.php
@@ -5,13 +5,16 @@
 	<title>Lista de usuarios</title>
 </head>
 	<body>
-		<a href="C_mostrarInsertUsuarios.php">Insertar usuario</a>
+		<a href="./Controller/C_mostrarInsertUsuarios.php">Insertar usuario</a>
    		<table>
    			<thead>
    				<tr>
    					<td>Usuarios</td>
+					<td>username</td>
    					<td>Constraseña</td>
    					<td>Nombre real</td>
+					<td></td>
+					<td></td>
    				</tr>
    			</thead>
    			<tbody>
@@ -21,6 +24,8 @@
    			    echo "<td>" . $usuario['username'] . "</td>";
    			    echo "<td>" . $usuario['password'] . "</td>";
    			    echo "<td>" . $usuario['nombre'] . "</td>";
+   			    echo "<td><a href='Controller/C_modificarUsuario.php?username=" . $usuario['username'] . "'>Modificar</a></td>";
+   			    echo "<td><a href='Controller/C_eliminarUsuario.php?username=" . $usuario['username'] . "'>Eliminar</a></td>";
    			    echo "</tr>";
    			}
    			
